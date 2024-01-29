bc2b4135de66ad38ed0e86f56d478756a8bc971e
chirtu <chitufatuma45@gmail.com>
2024-01-29 16:34:02 +0300

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        redirectTo: "/",
+        pathMatch: "full"
+    },
+    {
+        path: " ",
+        component:HomeComponent
+    },
+    {
+        path: "my-profile",
+        component: UserProfileComponent
+    }
+];
